1affa0c1a81ddedfa19ff821a328b995b20d74bd
aashmanarora14 <137777040+aashmanarora14@users.noreply.github.com>
2024-08-23 10:58:19 +0530

    Update app_start.sh

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -3,9 +3,4 @@ cd /home/ubuntu/pipeline-deploy
 
 npm run build
 
-if ! command -v pm2 &> /dev/null
-then
-    sudo npm install -g pm2
-fi
-pm2 start npm --name "react-app" -- start
-pm2 save
+npm run start
