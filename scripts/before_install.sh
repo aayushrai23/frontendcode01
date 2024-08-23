a9175132637185cebe9d10bf4af1322ee16fa812
aashmanarora14 <137777040+aashmanarora14@users.noreply.github.com>
2024-08-23 10:34:03 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+
+sudo apt-get update
+
+if ! command -v node &> /dev/null
+then
+    curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+    sudo apt-get install -y nodejs
+fi
+
 cd /home/ubuntu/pipeline-deploy
-apt-get update -y
+
+sudo rm -rf node_modules/ build/
+
