0077f8a1ad3db9ed016567f15fda0f79e61b40d0
aashmanarora14 <137777040+aashmanarora14@users.noreply.github.com>
2024-08-26 14:51:53 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-<<<<<<< HEAD
 cd /home/ubuntu/frontend
-npm install --force
-npm run build
-=======
-cd /home/ubuntu/pipeline-deploy
 npm install
->>>>>>> 74418a5965852ae9ddc693c3aa440290b833350b
