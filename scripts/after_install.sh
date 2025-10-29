667d90d381496fb8f22781a520f799796114b2fc
Aayush Rai <101255497+aayushrai23@users.noreply.github.com>
2025-10-29 10:44:44 +0530

    Delete scripts directory

--- scripts/after_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /home/ubuntu/frontend-code
-rm -rf /home/ubuntu/frontend-code/node_modules
-
-npm install
-npm run build
-
--- scripts/app_start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/ubuntu/frontend-code
-pm2 delete react
-pm2 start npm --name "react" -- start
-pm2 restart react
